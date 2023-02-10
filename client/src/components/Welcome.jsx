08cdf4a68a4e5bd79ff975dd1ebc042d6d4afb1f
prejin2310 <prejin.pr23@gmail.com>
2023-02-10 20:17:33 +0530

    blockchain transaction & storing working

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -17,11 +17,16 @@ const Input = ({placeholder,name,type,value,handleChange}) => (
 );
 
 const Welcome = () => {
-  const {value}=useContext(TransactionContext);
-  console.log(value);
+  
+  const {connectWallet,currentAccount,formData,sendTransaction,handleChange}=useContext(TransactionContext);
 
-    const connectWallet=() => {}
-    const handleSubmit = () => {}
+
+    const handleSubmit = (e) => {
+      const {addressTo,amount,keyword,message}=formData;
+      e.preventDefault();
+      if(!addressTo || !amount || !keyword || !message) return;
+      sendTransaction();
+    }
 
 
     return (
@@ -31,10 +36,12 @@ const Welcome = () => {
                 <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">Send Crypto <br /> across the world</h1>
                 <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto</p>
         
-                  <button type="button" onClick={connectWallet} className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                  {!currentAccount && (<button type="button" 
+                    onClick={connectWallet} 
+                    className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
                     <AiFillPlayCircle className="text-white mr-2" />
                     <p className="text-white text-base font-semibold"> Connect Wallet </p>
-                  </button>
+                  </button>)}
 
                   <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
                       <div className={`rounded-tl-2xl ${commonStyles}`}>Reaiability</div>
@@ -66,10 +73,10 @@ const Welcome = () => {
               </div>
 
               <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-                <Input placeholder="Address To" name="addressTo" type="text" handleChange={() =>{}} />
-                <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() =>{}} />
-                <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() =>{}} />
-                <Input placeholder="Enter Message" name="message" type="text" handleChange={() =>{}} />
+                <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+                <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+                <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+                <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
                 <div className="h-[1px] w-full bg-gray-400 my-2"/>
                 {false ? (
                    <Loader/>
--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -7,7 +7,7 @@ export const TransactionContext = React.createContext();
 
 const { ethereum } = window;
 
-const createEthereumContract = () => {
+const getEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum);
   const signer = provider.getSigner();
   const transactionsContract = new ethers.Contract(contractAddress, contractABI, signer);
@@ -16,17 +16,86 @@ const createEthereumContract = () => {
 };
 
 export const TransactionsProvider = ({ children }) => {
- const checkIfWalletIsConnected = async () => {
-    if(!ethereum) return alert("Please install metamask");
-    const accounts=await ethereum.request({method: 'eth_accounts'});
-    console.log(accounts);
+
+ const [currentAccount, setCurrentAccount] = useState("");
+ const [formData, setFormData] = useState({addressTo:'',amount:'',keyword:'',message:''});
+ const [isLoading,setIsLoading]=useState(false);
+ const [transactionCount, setTransactionCount] = useState(localStorage.getItem("transactionCount"));
+
+
+ const handleChange=(e,name) => {
+    setFormData((prevState) => ({ ...prevState,[name]: e.target.value}));
  }
+
+ const checkIfWalletIsConnected = async () => {
+    try{
+        if(!ethereum) return alert("Please install metamask");
+    const accounts=await ethereum.request({method: 'eth_accounts'});
+    if(accounts.length){
+        setCurrentAccount(accounts[0]);
+        //getAllTransactions();
+    } else {
+        console.log("No Accounts Found");
+    }
+
+    }catch (error){
+        console.log(error);
+        throw new Error("No ethereum object.")
+    }
+ }
+
+ const connectWallet=async () => {
+    try {
+        if(!ethereum) return alert("Please install metamask");
+        const account = await ethereum.request({method: 'eth_requestAccounts'});
+        setCurrentAccount(account[0]);
+    } catch (error) {
+        console.log(error);
+        throw new Error("No ethereum object.")
+ }
+}
+
+const sendTransaction=async () => {
+    try {
+        if(!ethereum) return alert("Please install metamask");
+        const {addressTo,amount,keyword,message}=formData;
+        const transactionsContract= getEthereumContract();
+        const parsedAmount=ethers.utils.parseEther(amount);
+
+        await ethereum.request({
+            method: 'eth_sendTransaction',
+            params: [{
+                from:currentAccount,
+                to:addressTo,
+                gas: '0x5208' ,//2100 gwei
+                value: parsedAmount._hex, //0.00001
+            }]
+        });
+        //to store transaction
+
+        const transactionHash=await transactionsContract.addToBlockchain(addressTo,parsedAmount,message,keyword);
+        setIsLoading(true);
+        console.log(`Loading -${transactionHash.hash}`);
+        await transactionHash.wait();
+
+        setIsLoading(flase);
+        console.log(`Sucess -${transactionHash.hash}`);
+
+        const transactionCount=await transactionsContract.getTransactionCount();
+        setTransactionCount(transactionCount.toNumber());
+
+    } catch (error) {
+        console.log(error);
+        throw new Error("No ethereum object.")
+    }
+}
+
 useEffect(() => {
     checkIfWalletIsConnected();
 },[]);
 
 return (
-    <TransactionContext.Provider value={{value: 'test'}}>
+    <TransactionContext.Provider value={{connectWallet,currentAccount,formData,setFormData,handleChange,sendTransaction}}>
         {children}
     </TransactionContext.Provider>
 );
